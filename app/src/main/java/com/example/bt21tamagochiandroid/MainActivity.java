615685fba729d47d1141f6e13e804751f47ff7e2
michelleykim <33190160+michelleykim@users.noreply.github.com>
2020-05-01 18:13:32 -0600

    From mainScreen to gameScreen translation implemented

--- app/src/main/java/com/example/bt21tamagochiandroid/MainActivity.java
+++ app/src/main/java/com/example/bt21tamagochiandroid/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.bt21tamagochiandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button continueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        continueButton = (Button) findViewById(R.id.continueButton);
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toGameScreen();
+            }
+        });
     }
+
+    private void toGameScreen() {
+        Intent intent = new Intent(this, GameScreen.class);
+        startActivity(intent);
+    }
+
+
 }
